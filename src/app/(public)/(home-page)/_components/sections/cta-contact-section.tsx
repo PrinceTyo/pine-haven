c79c71e80a4da871a3385b20df3bffadbf00a8f6
PrinceTyo <kresnaale2@gmail.com>
2026-06-17 19:50:05 +0700

    feat: add cta-contact-section

---
+++ src/app/(public)/(home-page)/_components/sections/cta-contact-section.tsx
@@ -0,0 +1,28 @@
+import Link from "next/link";
+import { IoIosArrowForward } from "react-icons/io";
+
+export default function CtaContactSection() {
+  return (
+    <div
+      className="relative h-116 md:h-96 flex items-center justify-center bg-fixed"
+      style={{
+        backgroundImage: `url("/cta-contact.jpg")`,
+        backgroundSize: "cover",
+        backgroundPosition: "center",
+      }}
+    >
+      <div className="absolute inset-0 bg-black/50"></div>
+      <div className="z-10 flex flex-col items-center justify-center text-center space-y-2">
+        <p className="text-white/90 tracking-widest uppercase">Pine Haven</p>
+        <h2 className="text-3xl md:text-5xl text-white font-medium">
+          Have Questions Or Feedback?
+        </h2>
+        <Link href="/room">
+          <button className="mt-6 cursor-pointer flex items-center gap-2 py-3 px-8 bg-transparent border border-white text-white text-md hover:bg-primary hover:border hover:border-primary hover:text-white transition duration-200">
+            Explore More <IoIosArrowForward />
+          </button>
+        </Link>
+      </div>
+    </div>
+  );
+}
--- src/app/(public)/(home-page)/page.tsx
+++ src/app/(public)/(home-page)/page.tsx
@@ -2,6 +2,7 @@ import HeroSection from "@/app/(public)/(home-page)/_components/sections/hero-se
 import RoomSection from "./_components/sections/room-section";
 import AboutSection from "@/app/(public)/(home-page)/_components/sections/about-section";
 import AmenitiesSection from "./_components/sections/amenities-section";
+import CtaContactSection from "./_components/sections/cta-contact-section";
 
 export default function Home() {
   return (
@@ -10,6 +11,7 @@ export default function Home() {
       <AboutSection />
       <AmenitiesSection />
       <RoomSection />
+      <CtaContactSection />
     </div>
   );
 }
