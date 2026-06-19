577765c55554d94d8f14b2bd6afe35d5e15065d8
PrinceTyo <kresnaale2@gmail.com>
2026-06-19 19:09:52 +0700

    fix(ui): contact-page

--- src/app/(public)/(other-page)/contact/_components/contact-form.tsx
+++ src/app/(public)/(other-page)/contact/_components/contact-form.tsx
@@ -3,7 +3,7 @@
 import { useActionState } from "react";
 import { ContactMessage } from "@/lib/actions";
 import clsx from "clsx";
-import { Field, FieldError, FieldLabel } from "@/components/ui/field";
+import { Field, FieldError } from "@/components/ui/field";
 import { Input } from "@/components/ui/input";
 import { Textarea } from "@/components/ui/textarea";
 
@@ -11,7 +11,9 @@ export default function ContactForm() {
   const [state, formAction, isPending] = useActionState(ContactMessage, null);
 
   return (
-    <div className="bg-white p-8 rounded-sm shadow-sm">
+    <div className="bg-white">
+      <p className="tracking-widest uppercase">Contact Us</p>
+      <h1 className="text-5xl font-semibold">Get in Touch</h1>
       {state?.message ? (
         <div
           className="p-4 mb-4 text-sm text-gray-800 rounded-lg bg-green-50"
@@ -21,15 +23,13 @@ export default function ContactForm() {
         </div>
       ) : null}
       <form action={formAction}>
-        <div className="grid md:grid-cols-2 gap-7 mt-6">
+        <div className="grid md:grid-cols-2 gap-7 mt-10">
           <Field>
-            <FieldLabel>Name</FieldLabel>
             <Input name="name" placeholder="John Doe" />
             <FieldError>{state?.error?.name}</FieldError>
           </Field>
 
           <Field>
-            <FieldLabel>Email</FieldLabel>
             <Input
               type="email"
               name="email"
@@ -39,13 +39,11 @@ export default function ContactForm() {
           </Field>
 
           <Field className="md:col-span-2">
-            <FieldLabel>Subject</FieldLabel>
             <Input name="subject" placeholder="Subject" />
             <FieldError>{state?.error?.subject}</FieldError>
           </Field>
 
           <Field className="md:col-span-2">
-            <FieldLabel>Message</FieldLabel>
             <Textarea name="message" rows={5} placeholder="Your message..." />
             <FieldError>{state?.error?.message?.[0]}</FieldError>
           </Field>
@@ -53,7 +51,7 @@ export default function ContactForm() {
         <button
           type="submit"
           className={clsx(
-            "mt-6 px-10 py-4 text-center font-semibold text-white w-full bg-orange-400 rounded-sm hover:bg-orange-500 transition duration-200 cursor-pointer",
+            "mt-6 px-10 py-4 text-center font-semibold text-white w-full bg-orange-400 hover:bg-orange-500 transition duration-200 cursor-pointer",
             {
               "opacity-50 cursor-progress animate-pulse": isPending,
             },
--- src/app/(public)/(other-page)/contact/page.tsx
+++ src/app/(public)/(other-page)/contact/page.tsx
@@ -8,53 +8,41 @@ import ContactForm from "./_components/contact-form";
 
 export default function ContactPage() {
   return (
-    <div>
+    <section>
       <HeaderPage title="Contact Us" subtitle="Lorem ipsum dolor sit amet." />
       <div className="max-w-7xl mx-auto py-20 px-4">
-        <div className="grid md:grid-cols-2 gap-8">
-          <div>
-            <h1 className="text-lg text-gray-500 mb-3">Contact Us</h1>
-            <h1 className="text-5xl font-semibold text-gray-900 mb-4">
-              Get In Touch Today
-            </h1>
-            <p className="text-gray-700 py-5">
-              Lorem ipsum dolor sit amet consectetur adipisicing elit. Ad magnam
-              dolorum dolore asperiores nisi excepturi.
-            </p>
-            <ul className="list-item space-y-6 pt-8">
-              <li className="flex gap-5">
-                <div className="flex-none bg-gray-300 p-3 shadow-sm rounded-sm">
-                  <IoMailOutline className="size-7" />
-                </div>
-                <div className="flex-1">
-                  <h4 className="text-lg font-semibold mb-1">Email :</h4>
-                  <p>email-us@example.com</p>
-                </div>
-              </li>
-              <li className="flex gap-5">
-                <div className="flex-none bg-gray-300 p-3 shadow-sm rounded-sm">
-                  <IoCallOutline className="size-7" />
-                </div>
-                <div className="flex-1">
-                  <h4 className="text-lg font-semibold mb-1">Phone Number :</h4>
-                  <p>+9973 4592 6744, +8299 6642 8323</p>
-                </div>
-              </li>
-              <li className="flex gap-5">
-                <div className="flex-none bg-gray-300 p-3 shadow-sm rounded-sm">
-                  <IoLocationOutline className="size-7" />
-                </div>
-                <div className="flex-1">
-                  <h4 className="text-lg font-semibold mb-1">Address :</h4>
-                  <p>Coderx Street Rood 2026, Hallstat, Austria</p>
-                </div>
-              </li>
-            </ul>
+        <div className="flex items-center justify-center gap-4">
+          <div className="w-80 text-center p-10 flex flex-col items-center justify-center gap-2 shadow-md border border-gray-100">
+            <IoLocationOutline className="size-12" />
+            <h2 className="text-xl font-semibold">Location</h2>
+            <p>Coderx Street Rood 2026, Hallstat, Austria</p>
           </div>
-          {/* Contact Form */}
-          <ContactForm />
+          <div className="bg-primary text-white w-80 text-center p-10 flex flex-col items-center justify-center gap-2 shadow-md border border-gray-100">
+            <IoMailOutline className="size-12" />
+            <h2 className="text-xl font-semibold">pinehaven@email.com</h2>
+            <p>Email us anytimefor anykind of quety.</p>
+          </div>
+          <div className="w-80 text-center p-10 flex flex-col items-center justify-center gap-2 shadow-md border border-gray-100">
+            <IoCallOutline className="size-12" />
+            <h2 className="text-xl font-semibold">Call: +8973 4592 6744</h2>
+            <p>Call us any kind suppor, we will wait for it</p>
+          </div>
+        </div>
+
+        <div className="mt-20 flex items-stretch gap-20">
+          <div className="w-1/2">
+            <iframe
+              src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3950.2250251129876!2d114.2203589741774!3d-8.078517980755175!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x2dd14b8e1921a729%3A0x3f4be9995a9d7a88!2sBobocabin%20Ijen%2C%20Banyuwangi!5e0!3m2!1sid!2sid!4v1781008480761!5m2!1sid!2sid"
+              className="w-full h-full grayscale"
+              loading="lazy"
+              allowFullScreen
+            ></iframe>
+          </div>
+          <div className="w-1/2">
+            <ContactForm />
+          </div>
         </div>
       </div>
-    </div>
+    </section>
   );
 }
