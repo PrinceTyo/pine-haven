7990d35d6663f75e37ddc16c9086796048a7271b
PrinceTyo <kresnaale2@gmail.com>
2026-07-09 09:42:47 +0700

    ui: enhance reservation-detail

--- src/app/(public)/(other-page)/myreservation/[id]/page.tsx
+++ src/app/(public)/(other-page)/myreservation/[id]/page.tsx
@@ -1,5 +1,6 @@
 import { Suspense } from "react";
 import ReservationDetail from "./_components/reservation-detail";
+import HeaderPage from "@/components/header/header-page";
 
 export default async function MyReservationDetail({
   params,
@@ -10,7 +11,11 @@ export default async function MyReservationDetail({
 
   return (
     <div className="min-h-screen bg-slate-50">
-      <div className="max-w-5xl mx-auto mt-10 py-20 px-4">
+      <HeaderPage
+        title="Reservation Detail"
+        subtitle="Lorem ipsum dolor sit amet."
+      />
+      <div className="max-w-5xl mx-auto py-20 px-4">
         <Suspense fallback={<p>Loading...</p>}>
           <ReservationDetail reservationId={reservationId} />
         </Suspense>
