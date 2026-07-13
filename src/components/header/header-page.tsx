fbf7d4ba3cbb1d041d20beae24217fd5e8f7746f
PrinceTyo <kresnaale2@gmail.com>
2026-07-13 19:57:08 +0700

    feat: add breadcrumb in HeaderPage component

--- src/components/header/header-page.tsx
+++ src/components/header/header-page.tsx
@@ -1,11 +1,25 @@
 import Image from "next/image";
+import Link from "next/link";
+import {
+  Breadcrumb,
+  BreadcrumbItem,
+  BreadcrumbLink,
+  BreadcrumbList,
+  BreadcrumbPage,
+  BreadcrumbSeparator,
+} from "@/components/ui/breadcrumb";
+
+type BreadcrumbItemType = {
+  label: string;
+  href?: string;
+};
 
 export default function HeaderPage({
   title,
-  subtitle,
+  breadcrumbs,
 }: {
   title: string;
-  subtitle: string;
+  breadcrumbs: BreadcrumbItemType[];
 }) {
   return (
     <header className="relative h-60 text-white overflow-hidden">
@@ -18,9 +32,37 @@ export default function HeaderPage({
         />
         <div className="absolute inset-0 bg-black opacity-50"></div>
       </div>
-      <div className="relative flex flex-col justify-center items-center h-60 text-center pt-14">
-        <h1 className="text-5xl font-bold leading-tight capitalize">{title}</h1>
-        <p className="text-xl text-gray-300">{subtitle}</p>
+      <div className="relative flex h-60 flex-col items-center justify-center pt-14 text-center">
+        <h1 className="text-5xl font-bold capitalize">{title}</h1>
+
+        <Breadcrumb className="mt-4">
+          <BreadcrumbList>
+            {breadcrumbs.map((item, index) => (
+              <div key={index} className="flex items-center">
+                <BreadcrumbItem>
+                  {item.href ? (
+                    <BreadcrumbLink asChild>
+                      <Link
+                        href={item.href}
+                        className="text-gray-300 hover:text-white"
+                      >
+                        {item.label}
+                      </Link>
+                    </BreadcrumbLink>
+                  ) : (
+                    <BreadcrumbPage className="text-white">
+                      {item.label}
+                    </BreadcrumbPage>
+                  )}
+                </BreadcrumbItem>
+
+                {index < breadcrumbs.length - 1 && (
+                  <BreadcrumbSeparator className="text-gray-400" />
+                )}
+              </div>
+            ))}
+          </BreadcrumbList>
+        </Breadcrumb>
       </div>
     </header>
   );
